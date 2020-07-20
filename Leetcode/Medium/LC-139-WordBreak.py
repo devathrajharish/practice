0b96dce7df9ff31b6e4987c36bffbb038c09ac6b
Harish Devathraj <devathrajharish@gmail.com>
2020-07-20 02:50:59 -0500

    word BReak

---
+++ Leetcode/Medium/LC-139-WordBreak.py
@@ -0,0 +1,22 @@
+def main():
+    def wordBreak(s, wordDict):
+        if len(s) == 0:
+            return True
+        
+        for x in wordDict:
+            prefix = s[0:len(x)]
+            result = False
+
+            if prefix == x:
+                result = wordBreak(s[len(x):], wordDict)
+            if result:
+                return True
+
+        return False
+
+    s = "fastracecars"
+    wordDict = ["fast", "cars", "car","race","astra","run"]
+    print(wordBreak(s, wordDict))
+
+if __name__ == '__main__':
+    main()
