fd4e2a6e8d12684f47d8c54780c2411d7672ca85
Harish Devathraj <devathrajharish@gmail.com>
2020-07-21 17:29:52 -0500

    first spark local program

--- sparktest.py
+++ sparktest.py
@@ -1,16 +1,23 @@
 import findspark
 findspark.init()
+from pyspark.context import SparkContext
 from pyspark.sql import SparkSession
 def main():
-    spark = SparkSession.builder \
-        .master("local") \
-        .appName("Word Count") \
-        .config("spark.some.config.option", "some-value") \
-        .getOrCreate()
-    print(spark)
-    l = [('Alice', 1)]
-    rdd = spark.createDataFrame(l).collect()
-    print(rdd)
+    # spark = SparkSession.builder \
+    #     .master("local") \
+    #     .appName("Word Count") \
+    #     .config("spark.some.config.option", "some-value") \
+    #     .getOrCreate()
+
+    # l = [('Alice', 1)]
+    # rdd = spark.createDataFrame(l).collect()
+    # print(rdd)
+    sc = SparkContext('local')
+    rdd = sc.textFile('example')
+    print(rdd.first())
+
+    
+    sc.stop()
 
 if __name__ == '__main__':
     main()
